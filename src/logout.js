94004c67c98be2ee71afe8097e173f08949e2438
propn <xulei33@msn.com>
2019-10-27 19:55:17 +0800

    init

---
+++ src/logout.js
@@ -0,0 +1,66 @@
+require('./themes/default/css/sas.css');
+import $ from 'jQuery';
+
+$(document).on('keydown', function(e)
+{
+    if (e.keyCode === 27)
+    {
+        document.location.href = '';
+        $('#openModal').hide();
+        return false;
+    }
+});
+
+function setSubmitUrl(form)
+    {
+        var urlHash = decodeURIComponent(window.location.hash);
+
+        if (urlHash && urlHash.indexOf("#") === -1)
+            urlHash = "#" + urlHash;
+
+        form.action = form.action + urlHash;
+        return true;
+    }
+
+    function are_cookies_enabled()
+    {
+        var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+        if (typeof navigator.cookieEnabled == "undefined")
+        {
+            document.cookie="testcookie";
+            cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+        }
+
+        if (!cookieEnabled)
+        {
+            document.getElementById("nocookie-message").setAttribute("aria-hidden", "false");
+            var username = document.getElementById("username");
+            username.setAttribute("aria-labelledby", username.getAttribute("aria-labelledby") + " nocookie-message");
+            $('#nocookie-message').show();
+        }
+    }
+
+    $(document).ready(function()
+    {
+        are_cookies_enabled();
+    });
+
+    function modality()
+    {
+        switch(location.hash)
+        {
+            case "#openModal" :
+                $('#nonModal :input').attr('disabled', true);
+                document.getElementById("nonModal").setAttribute("aria-hidden","true");
+                document.getElementById("openModal").setAttribute("aria-hidden","false");
+                break;
+            default :
+                $('#nonModal :input').attr('disabled', false);
+                document.getElementById("nonModal").setAttribute("aria-hidden","false");
+                document.getElementById("openModal").setAttribute("aria-hidden","true");
+                break;
+        }
+    }
+    window.onhashchange = modality;
+    modality();
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');//清理built目录
+const CopyWebpackPlugin = require('copy-webpack-plugin');//复制静态图片资源
+const HtmlWebpackPlugin = require('html-webpack-plugin');//引入html-webpack-plugin
+const ExtractTextPlugin = require('extract-text-webpack-plugin');//css文件
+const SVGURLLOADER = require('svg-url-loader');//处理svg图片
+module.exports = { 
+    entry: {
+        index:'./src/index.js',
+        logout:'./src/logout.js',
+    },
+    output: {
+        filename:'[name].[hash:8].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: { // 处理对应模块
+        rules: [
+            {
+                test: /\.css$/, //提取css到单独文件
+                use: ExtractTextPlugin.extract({
+                  fallback: "style-loader",
+                  use: "css-loader",
+                })
+            },
+            { test: /\.(ttf|eot|woff|woff2)$/,// 处理字体文件的loader 
+                 use: 'url-loader' 
+            }, 
+            {
+                test:/\.(png|jpg|gif)$/,
+                use:[{
+                    loader:'url-loader',
+                    options:{
+                        outputPath:'images/',//输出到images文件夹
+                        limit:1,  //是把小于500B的文件打成Base64的格式，写入JS
+                        name:'[name].[ext]',
+                    }
+                }]
+            },
+			{
+			    test: /\.svg/,
+			    use: {
+			        loader: 'svg-url-loader',
+			        options: {}
+			    }
+			}
+        ]
+    },
+	plugins: [// 对应的插件
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([ //处理html的<img标签图片资源
+            { from: __dirname+'/src/themes', to: __dirname+'/dist/themes', },
+        ]),
+        new ExtractTextPlugin("styles.css"),
+        new HtmlWebpackPlugin({ //配置
+            title: 'SAS® Logon Manager',
+            favicon: 'src/themes/default/images/favicon.ico',
+            filename: 'index.html',//输出文件名
+            template: './src/index.html',//以当前目录下的index.html文件为模板生成dist/index.html文件
+			chunks: ['index'], // 默认会将打包出的所有 js 插入 html。故需指明页面需要的模块
+        }),
+        new HtmlWebpackPlugin({ //配置
+            title: 'SAS® Logon Manager',
+            favicon: 'src/themes/default/images/favicon.ico',
+            filename: 'logout.html',//输出文件名
+            template: './src/logout.html',//以当前目录下的index.html文件为模板生成dist/index.html文件
+			chunks: ['logout'], // 默认会将打包出的所有 js 插入 html。故需指明页面需要的模块
+        }),
+    ],    
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),//开发服务运行时的文件根目录
+        historyApiFallback: false,//true默认打开index.html，false会出现一个目录
+        watchContentBase: true,//热更新
+        host: 'localhost',//主机地址
+        port: 9090,//端口号
+        compress: true//开发服务器是否启动gzip等压缩
+    },
+}
